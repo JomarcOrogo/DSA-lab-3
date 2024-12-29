d45242494d2b8924a16ede4c21126dcf75cd1d6c
JomarcOrogo <jomarcorogo07@gmail.com>
2024-12-30 00:43:17 +0800

    Nanotime added

--- src/MaximumValue.java
+++ src/MaximumValue.java
@@ -2,19 +2,42 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-
+// Jomarc Orogo
 public class MaximumValue {
     public static void main(String[] args) {
         // Make this less if your machine might encounter an out-of-memory error
-        final int N = 10; // Try with smaller values like 1000, 10000, or 100000 for testing
+        final int N = 15; // Try with smaller values like 1000, 10000, or 100000 for testing
         int[] arr = addElements(N);
+        long start, end;
+
+        System.out.println("Jomarc Orogo");
         System.out.println(Arrays.toString(arr));
-        System.out.println("\n\nSimple Loop\t\t: " + findMaxSimpleLoop(arr));
-        System.out.println("Max Stream\t\t: " + findMaxStream(arr));
-        System.out.println("Max Collection\t: " + findMaxCollections(arr));
-        System.out.println("Max Parallel\t: " + findMaxParallel(arr));
-        System.out.println("Max Sorting\t\t: " + findMaxSorting(arr, N));
-        System.out.println("Maximum Value\t: " + findMaxRecursion(arr, N));
+        start = System.nanoTime();
+        int maxSimpleLoop = findMaxSimpleLoop(arr);
+        end = System.nanoTime();
+        System.out.println("\n\nSimple Loop\t\t: " + maxSimpleLoop + " (Time: " + (end - start) + " ns)");
+        start = System.nanoTime();
+        int maxStream = findMaxStream(arr);
+        end = System.nanoTime();
+        System.out.println("\nMax Stream\t\t: " + maxStream + " (Time: " + (end - start) + " ns)");
+        start = System.nanoTime();
+        int maxCollections = findMaxCollections(arr);
+        end = System.nanoTime();
+        System.out.println("\nMax Collection\t: " + maxCollections + " (Time: " + (end - start) + " ns)");
+        start = System.nanoTime();
+        int maxParallel = findMaxParallel(arr);
+        end = System.nanoTime();
+        System.out.println("\nMax Parallel\t: " + maxParallel + " (Time: " + (end - start) + " ns)");
+        start = System.nanoTime();
+        int maxSorting = findMaxSorting(arr, N);
+        end = System.nanoTime();
+        System.out.println("\nMax Sorting\t\t: " + maxSorting + " (Time: " + (end - start) + " ns)");
+        /*
+        start = System.nanoTime();
+        int maxRecursion = findMaxRecursion(arr, N);
+        end = System.nanoTime();
+        System.out.println("\nMaximum Value\t: " + maxRecursion + " (Time: " + (end - start) + " ns)");
+        */
     }
 
     // Method to generate random elements in an array
@@ -58,11 +81,13 @@ public class MaximumValue {
         }
     }
 
+    /*
     public static int findMaxRecursion(int[] arr, int N) {
         if (N == 1) {
             return arr[0];
         }
         return Math.max(arr[N-1], findMaxRecursion(arr, N - 1));
     }
+     */
 
 }
